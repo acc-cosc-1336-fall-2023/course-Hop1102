6572962eee69ee11546bbab85905db3710b13a3d
Hop1102 <143463844+Hop1102@users.noreply.github.com>
2023-09-17 00:12:17 +0000

    homework 2 Haley Hopper If Elif else

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,22 @@
-#
+
+ratio = 0
+
+def get_options_ratio(options, total):
+    ratio = options / total 
+    return ratio 
+
+def get_faculty_rating(ratio):
+    if (ratio <= 1 and ratio >= 0.9):
+        return 'Excellent'
+    elif (ratio < 0.9 and ratio >= 0.8):
+        return 'Very Good'
+    elif (ratio < 0.8 and ratio >= 0.7):
+        return 'Good'
+    elif (ratio < 0.7 and ratio >= 0.6):
+        return 'Needs Improvement'
+    elif (ratio < 0.6 and ratio >= 0.0):
+        return 'Unacceptable'
+    else:
+        ratio = 'Invalid Input' 
+
+
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,20 @@
-#
+#homework 2
+
+import decisions
+
+option = int(input('Enter a numerical option for ratio:'))
+
+total = int(input('Enter a numerical total for ratio:'))
+
+result = decisions.get_options_ratio(option, total) 
+
+print(result)
+
+value = decisions.get_faculty_rating(result)
+
+print(value)
+
+
+
+
+
