9ae87f0a3d3bb1538f56affb640d32e9d71f928f
Hop1102 <143463844+Hop1102@users.noreply.github.com>
2023-09-17 00:15:00 +0000

    practice

--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,7 +1,2 @@
 def get_number(num):
     return num
-
-def multiply_number(num1, num2):
-    result = num1 * num2
-    return result 
-
--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -15,6 +15,43 @@ class Test_Config(unittest.TestCase):
     def test_get_number_2(self):
         #test that the function get_number returns 2
         self.assertEqual(2, get_number(2))
+         def test_configuration(self):
+        self.asstEqual(True, test_config())
+        
+    def test_and_truth_tables(self):
+        self.assertEqual(True, get_and_result(True, True))
+        self.assertEqual(False, get_and_result(True, False))
+        self.assertEqual(False, get_and_result(True, True))
+        self.assertEqual(False, get_and_result(True, True))
+
+    def test_or_truth_tables(self):
+        self.assertEqual(True, get_or_result(True, True))
+        self.assertEqual(True, get_or_result(True, False))
+        self.assertEqual(True, get_or_result(False, True))
+        self.assertEqual(False,get_or_result(False, False))
+
+    def test_not_truth_tables(self):
+        self.assertEqual(True, get_not_value(False))
+        self.assertEqual(True, get_not_value(True))
+
+    def test_is_even(self):
+        self.assertEqual(False, is_even(1))
+        self.assertEqual(True, is_even(2))
+        self.assertEqual(False, is_even)
+
+    def test_is_vowel(self): 
+        self.assertEqual(True, is_vowel('a')) 
+        self.assertEqual(False, is_vowel('e'))
+        self.assertEqual(False, is_vowel('i'))
+        self.assertEqual(False, is_vowel('o'))
+        self.assertEqual(False, is_vowel('u'))
+
+    def test_is_consonant(self):
+        self.assertEqual(False, is_consonant('a'))
+        self.assertEqual(True, is_consonant('b'))
+
+
+    
 
     def test_get_multiply_numbers(self):
         self.assertEqual(25, multiply_number(5, 5)) 
